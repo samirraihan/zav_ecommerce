25f6537497b9ae9a5deaa1492861c2a019a0a00f
samirraihan <samirraihan93@gmail.com>
2026-02-24 13:23:39 +0600

    feat: update API route to sync user data with AuthSyncController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthSyncController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->get('/sync-user', [AuthSyncController::class, 'sync']);
